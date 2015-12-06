02a140a337632d8ee38fc5e5e676d3beb996f67f
Juli Emmel <justjiffy@gmail.com>
2015-12-06 14:38:43 -0800

    home link and refresh of typed.js working, cursor also showing, added who section copy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,51 +1,53 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 
+//DOCUMENT.READY
 $(function(){
-    $("h3#welcome").typed({
+    $('#welcome').typed({
         strings: ["Welcome :)", "You've landed at Juli's Online Portfolio"],
-        typeSpeed: 10,
-        showCursor: false,
-        backDelay: 2000,
-        contentType: 'html'
-});
+        typeSpeed: 30,
+        backDelay: 500,
+        contentType: 'html',
+    });
 
-// SCROLL ANIMATION
+    $('.reset').click(function(e) {
+        e.preventDefault();
+        scrollTo('#home');
+        newTyped();
+      });
 
-  function scrollTo(location) {
-    $('html, body').animate({scrollTop: $(location).offset().top}, 500, function() { console.log("Finished animating");});
-  }
+    // SCROLL ANIMATION
 
-  $('#who-nav').on('click', function(e) {
-    e.preventDefault();
-    scrollTo('#who');
+      function scrollTo(location) {
+        $('html, body').animate({scrollTop: $(location).offset().top}, 500, function() { console.log("Finished animating");});
+      }
+
+      $('#who-nav').on('click', function(e) {
+        e.preventDefault();
+        scrollTo('#who');
+      });
+      
+      $('#what-nav').on('click', function(e) {
+        e.preventDefault();
+        scrollTo('#what');
+      });
+
+      $('#where-nav').on('click', function(e) {
+        e.preventDefault();
+        scrollTo('#where');
+      });
+    // scroll animation - end
+}); // document.ready - end
+
+function newTyped() {
+  $('#welcome').hide();
+  $('.typed-cursor').hide();
+  $('#rewelcome').typed({
+      strings: ['Back for more fun typing animations???', 'Check out typed.js to create your own.', 'More tricks coming soon... &lt;3&#47;  JULI'],
+      typeSpeed: 30,
+      backDelay: 500,
+      contentType: 'html',
   });
-  
-  $('#what-nav').on('click', function(e) {
-    e.preventDefault();
-    scrollTo('#what');
-  });
-
-  $('#where-nav').on('click', function(e) {
-    e.preventDefault();
-    scrollTo('#where');
-  });
-
-});
-// scroll animation - end
-
-
+ }
