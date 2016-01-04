f0a300d805c02bdb514f6f19fbbb431e158ea08f
Juli Emmel <justjiffy@gmail.com>
2016-01-04 14:46:14 -0800

    removed document.ready from script

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-$(document).ready(function(){
-  $('.lightswitch').on('click', function() {
-    console.log('clicked');
-    $(this).addClass('hide').removeClass('show');
-    $('.lightswitchoff').addClass('show').removeClass('hide');
-    $('.justjiffy').addClass('hide').removeClass('show');
-    $('.justjiffyoff').addClass('show').removeClass('hide');
-    $('.nav').addClass('hide').removeClass('show');
-    $('.navoff').addClass('show').removeClass('hide');
-    $('body').addClass('black');
-   });
+$('.lightswitch').on('click', function() {
+  console.log('clicked');
+  $(this).addClass('hide').removeClass('show');
+  $('.lightswitchoff').addClass('show').removeClass('hide');
+  $('.justjiffy').addClass('hide').removeClass('show');
+  $('.justjiffyoff').addClass('show').removeClass('hide');
+  $('.nav').addClass('hide').removeClass('show');
+  $('.navoff').addClass('show').removeClass('hide');
+  $('body').addClass('black');
+ });
 
-  $('.lightswitchoff').on('click', function() {
-     console.log('clicked');
-    $(this).removeClass('show').addClass('hide');
-    $('.lightswitch').removeClass('hide').addClass('show');
-    $('.justjiffyoff').removeClass('show').addClass('hide');
-    $('.justjiffy').removeClass('hide').addClass('show');
-    $('.navoff').removeClass('show').addClass('hide');
-    $('.nav').removeClass('hide').addClass('show');
-    $('body').removeClass('black');
-   });
-});
+$('.lightswitchoff').on('click', function() {
+   console.log('clicked');
+  $(this).removeClass('show').addClass('hide');
+  $('.lightswitch').removeClass('hide').addClass('show');
+  $('.justjiffyoff').removeClass('show').addClass('hide');
+  $('.justjiffy').removeClass('hide').addClass('show');
+  $('.navoff').removeClass('show').addClass('hide');
+  $('.nav').removeClass('hide').addClass('show');
+  $('body').removeClass('black');
+ });
